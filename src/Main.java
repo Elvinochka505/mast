afa827228f41afc940da3e01f061a3ac7274ada1
Elvina <khabibullinaelvina@mail.ru>
2025-03-02 21:35:37 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 15_000;
+        int bonus = 20;
+        int x;
+        x = amount / bonus;
+        System.out.println(x);
+    }
+}
