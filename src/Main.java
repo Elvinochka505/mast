f987f0c720add65f463d09e7886648bb6c681b73
Elvina <khabibullinaelvina@mail.ru>
2025-03-16 20:22:11 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int amount = 15_000;
         int bonus = 20;
-        int miles;
-        miles = amount / bonus;
+        int miles = amount / bonus;
         System.out.println(miles);
     }
 }
